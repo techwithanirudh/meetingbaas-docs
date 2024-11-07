95403aabfeed0f2e799a413605f9556b6fcae0bd
Anirudh Sriram <hello@techwithanirudh.com>
2024-11-07 16:38:46 +0000

    fix: icons

--- lib/source.ts
+++ lib/source.ts
@@ -3,9 +3,19 @@ import { loader } from 'fumadocs-core/source';
 import { attachFile, createOpenAPI } from 'fumadocs-openapi/server';
 import { docs, meta } from '@/.source';
 
+import { icons } from "lucide-react";
+import { createElement } from "react";
+import { IconContainer } from "@/components/ui/icon";
+
 export const source = loader({
   baseUrl: '/',
   source: createMDXSource(docs, meta),
+  icon(icon) {
+    if (icon && icon in icons)
+      return createElement(IconContainer, {
+        icon: icons[icon as keyof typeof icons],
+      });
+  },
   pageTree: {
     attachFile,
   },
