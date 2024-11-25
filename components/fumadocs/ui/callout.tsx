1c40fbbe94d0f09eea4bf5c56283b749b84f335f
Lazare Rossillon <lazrossi@student.42.fr>
2024-11-25 12:30:26 +0100

    feat: custom Callout component with correct colors, borders, line \n

---
+++ components/fumadocs/ui/callout.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/fumadocs/cn";
+import { InfoIcon } from "lucide-react";
+import type { HTMLAttributes, ReactNode } from "react";
+
+interface CalloutProps extends HTMLAttributes<HTMLDivElement> {
+  type?: "info" | "warning" | "error";
+  children: ReactNode;
+  border?: boolean;
+}
+
+export function Callout({
+  children,
+  type = "info",
+  className,
+  border = false,
+  ...props
+}: CalloutProps): React.ReactElement {
+  return (
+    <div
+      className={cn(
+        "my-4 flex items-baseline gap-2 rounded-lg bg-fd-card p-3",
+        border && [
+          "border",
+          type === "info" && "border-[#78FFF0]",
+          type === "warning" && "border-[#FFFF93]",
+          type === "error" && "border-red-500",
+        ],
+        type === "info" && "[&_svg]:text-[#78FFF0]",
+        type === "warning" && "[&_svg]:text-[#FFFF93]",
+        type === "error" && "[&_svg]:text-red-500",
+        "[&>div>p+ul]:mt-0 [&>div>p]:mb-0",
+        className
+      )}
+      {...props}
+    >
+      <InfoIcon className="h-4 w-4 shrink-0 translate-y-[2px]" />
+      <div>{children}</div>
+    </div>
+  );
+}
