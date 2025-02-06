7e9cd0ed49bb5f08e2fd56599f1b4f7499fdc692
Anirudh Sriram <hello@techwithanirudh.com>
2025-02-06 13:10:14 +0000

    fix: callout styling

--- components/fumadocs/callout.tsx
+++ components/fumadocs/callout.tsx
@@ -24,17 +24,21 @@ export const Callout = forwardRef<HTMLDivElement, CalloutProps>(
       <div
         ref={ref}
         className={cn(
-          "my-6 flex flex-row gap-2 rounded-lg border bg-fd-card p-3 text-sm text-fd-card-foreground shadow-md",
-          className
+          "my-6 flex flex-row gap-2 rounded-lg border bg-fd-card p-3 text-sm text-fd-card-foreground shadow-xs",
+          className,
+          {
+            error: "border-red-500/50 text-red-600",
+            warn: "border-amber-500/50 text-amber-600",
+            info: "border-blue-500/50 text-blue-600",
+          }[type]
         )}
         {...props}
       >
         {icon ??
           {
-            // todo
-            info: <Info className="h-5 w-5 text=yellow" />,
-            warn: <AlertTriangle className="h-5 w-5 text-callout-warn" />,
-            error: <CircleX className="h-5 w-5 text-callout-error" />,
+            info: <Info className="h-5 w-5 text-blue-600" />,
+            warn: <AlertTriangle className="h-5 w-5 text-amber-600" />,
+            error: <CircleX className="h-5 w-5 text-red-600" />,
           }[type]}
         <div className="min-w-0 flex-1">
           {title ? <p className="not-prose mb-2 font-medium">{title}</p> : null}
