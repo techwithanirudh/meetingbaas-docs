32a44acd8cc1514ffae26882e2b6697c294d8ea7
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-07 16:15:05 +0000

    fix: a few bugs

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -48,7 +48,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         className="absolute inset-0 z-[-1] h-[64rem] max-h-screen overflow-hidden"
         style={{
           backgroundImage:
-            "radial-gradient(49.63% 57.02% at 58.99% -7.2%, hsl(var(--primary)/0.1) 39.4%, transparent 100%)",
+            "radial-gradient(49.63% 57.02% at 58.99% -7.2%, hsl(var(--color-fd-primary)/0.1) 39.4%, transparent 100%)",
         }}
       >
         <svg
@@ -78,7 +78,7 @@ export default function Layout({ children }: { children: ReactNode }) {
               fill="url(#radial-dark)"
             />
           </mask>
-          <g mask="url(#mask-dark)" className="fill-primary">
+          <g mask="url(#mask-dark)" className="fill-fd-primary">
             <path
               d="M506.419 281.855L446.417 297.931V359.885L506.419 343.71V281.855Z"
               fillOpacity="0.05"
