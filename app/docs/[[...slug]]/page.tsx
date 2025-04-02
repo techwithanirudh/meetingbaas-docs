8960ef37d74e1f02e69c1a14e801817312905a87
Lazare Rossillon <lazrossi@student.42.fr>
2025-04-02 10:46:56 +0200

    betteropen.json + try markdown

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,23 +1,30 @@
-import { source, openapi } from '@/lib/source';
-import {
-  DocsPage,
-  DocsBody,
-  DocsDescription,
-  DocsTitle,
-} from 'fumadocs-ui/page';
-import { notFound } from 'next/navigation';
-import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { createMetadata } from '@/lib/metadata';
 import { metadataImage } from '@/lib/metadata-image';
+import { openapi, source } from '@/lib/source';
+import defaultMdxComponents from 'fumadocs-ui/mdx';
+import {
+  DocsBody,
+  DocsDescription,
+  DocsPage,
+  DocsTitle,
+} from 'fumadocs-ui/page';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { notFound } from 'next/navigation';
 
-export default async function Page(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
+const mdxComponents = {
+  ...defaultMdxComponents,
+  APIPage: openapi.APIPage,
+};
+
+type PageProps = {
+  params: { slug: string[] };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function Page({ params }: PageProps) {
+  const slugs = params.slug || [];
+  const page = source.getPage(slugs);
   if (!page) notFound();
-
-  // const path = `content/docs/${page.file.path}`;
 
   const MDX = page.data.body;
 
@@ -29,19 +36,20 @@ export default async function Page(props: {
         style: 'clerk',
         single: false,
       }}
-      // editOnGithub={{
-      //   repo: "docs",
-      //   owner: "Meeting-Baas",
-      //   sha: "main",
-      //   path,
-      // }}
     >
       <DocsTitle>{page.data.title}</DocsTitle>
-      <DocsDescription>{page.data.description}</DocsDescription>
+      {page.data.description && /[#*`\[\]]/g.test(page.data.description) ? (
+        <DocsBody>
+          <MDXRemote
+            source={page.data.description}
+            components={mdxComponents}
+          />
+        </DocsBody>
+      ) : (
+        <DocsDescription>{page.data.description}</DocsDescription>
+      )}
       <DocsBody>
-        <MDX
-          components={{ ...defaultMdxComponents, APIPage: openapi.APIPage }}
-        />
+        <MDX components={mdxComponents} />
       </DocsBody>
     </DocsPage>
   );
@@ -51,11 +59,9 @@ export async function generateStaticParams() {
   return source.generateParams();
 }
 
-export async function generateMetadata(props: {
-  params: Promise<{ slug?: string[] }>;
-}) {
-  const params = await props.params;
-  const page = source.getPage(params.slug);
+export async function generateMetadata({ params }: PageProps) {
+  const slugs = params.slug || [];
+  const page = source.getPage(slugs);
   if (!page) notFound();
 
   const description =
