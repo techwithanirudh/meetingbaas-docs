acdc952bfc5f3ffceb713966c2f7755a2222074e
Anirudh Sriram <hello@techwithanirudh.com>
2025-04-02 12:59:57 +0000

    fix: oxc

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,6 @@
 import {
   defineConfig,
   defineDocs,
-  defineCollections,
   frontmatterSchema,
   metaSchema,
 } from 'fumadocs-mdx/config';
@@ -9,7 +8,6 @@ import { transformerTwoslash } from 'fumadocs-twoslash';
 import { createFileSystemTypesCache } from 'fumadocs-twoslash/cache-fs';
 import remarkMath from 'remark-math';
 import { fileGenerator, remarkDocGen, remarkInstall } from 'fumadocs-docgen';
-import { remarkTypeScriptToJavaScript } from 'fumadocs-docgen/remark-ts2js';
 import rehypeKatex from 'rehype-katex';
 import { z } from 'zod';
 import { rehypeCodeDefaultOptions } from 'fumadocs-core/mdx-plugins';
@@ -75,7 +73,6 @@ export default defineConfig({
       remarkAutoTypeTable,
       [remarkInstall, { persist: { id: 'package-manager' } }],
       [remarkDocGen, { generators: [fileGenerator()] }],
-      remarkTypeScriptToJavaScript,
     ],
     rehypePlugins: (v) => [rehypeKatex, ...v],
   },
